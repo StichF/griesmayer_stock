352596504aa1d92030d0ba940936d6507a991b48
StichF <sti04455@spengergasse.at>
2021-05-23 10:48:39 +0200

    Customers

---
+++ src/Customers.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Customers
+{
+	private ArrayList<Customer> customers;
+	
+	public Customers()
+	{
+		customers = new ArrayList<>();
+	}
+
+	public ArrayList<Customer> getCustomers()
+	{
+		return customers;
+	}
+
+	public void setCustomers(ArrayList<Customer> customers)
+	{
+		this.customers = customers;
+	}
+	
+	
+}
